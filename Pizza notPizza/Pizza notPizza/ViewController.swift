a161e41923a8f5403aa3505f64ac3ebd8c71da43
sakadalim <sakadalim@gmail.com>
2017-07-19 20:26:30 -0400

    Added working mvp
    removed second view controller. everything is done one view controller.

--- Pizza notPizza/Pizza notPizza/ViewController.swift
+++ Pizza notPizza/Pizza notPizza/ViewController.swift
@@ -8,110 +8,112 @@
 
 import UIKit
 import Clarifai
-import SwiftyJSON
+
 
 
 class ViewController: UIViewController {
     
-    let picker = UIImagePickerController()
+    @IBOutlet weak var imageView: UIImageView!
     
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    let picker = UIImagePickerController()
+
     var app:ClarifaiApp?
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        app = ClarifaiApp(apiKey: "bdb50c049f0646eeaf05bf744570a452")
         picker.delegate = self
+        app = ClarifaiApp(apiKey: "bdb50c049f0646eeaf05bf744570a452")
         
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if let identifier = segue.identifier {
-            if identifier == "cameraTapped" {
-                
-                if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
-                    picker.allowsEditing = false
-                    picker.sourceType = UIImagePickerControllerSourceType.camera
-                    picker.cameraCaptureMode = .photo
-                    present(picker, animated: true, completion: nil)
-                }else{
-                    let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .alert)
-                    let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
-                    alert.addAction(ok)
-                    present(alert, animated: true, completion: nil)
-                }
-                
-                
-                
-                
-            } else if identifier == "photosTapped" {
-                
-                picker.allowsEditing = false
-                picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
-                present(picker, animated: true, completion: nil)
-                
-        
-                let resultViewController = segue.destination as! ResultViewController
-//                resultViewController.imageView.image =
-                
-                
-            }
-            
+    @IBAction func photosTapped(_ sender: Any) {
+        picker.allowsEditing = false;
+        picker.sourceType = UIImagePickerControllerSourceType.photoLibrary
+        picker.delegate = self;
+        present(picker, animated: true, completion: nil)
+
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+        if(UIImagePickerController .isSourceTypeAvailable(UIImagePickerControllerSourceType.camera)){
+            picker.allowsEditing = false
+            picker.sourceType = UIImagePickerControllerSourceType.camera
+            picker.cameraCaptureMode = .photo
+            present(picker, animated: true, completion: nil)
+        }else{
+            let alert = UIAlertController(title: "Camera Not Found", message: "This device has no Camera", preferredStyle: .alert)
+            let ok = UIAlertAction(title: "OK", style:.default, handler: nil)
+            alert.addAction(ok)
+            present(alert, animated: true, completion: nil)
         }
     }
     
-    
-    //1 Research programmatic segues 
-    //2 When imagedidfinishpickingmedia... , perform segue 
-    //3 when performing segue, set destination's image view image to be the selected image
-    
-    func predictImage(image: UIImage){
+    func recognizeImage(image: UIImage) {
+        
+        // Check that the application was initialized correctly.
         if let app = app {
+            
+            // Fetch Clarifai's general model.
             app.getModelByID("bd367be194cf45149e75f01d59f77ba7", completion: { (model, error) in
+                
+                // Create a Clarifai image from a uiimage.
                 let caiImage = ClarifaiImage(image: image)!
+                
+                // Use Clarifai's general model to pedict tags for the given image.
                 model?.predict(on: [caiImage], completion: { (outputs, error) in
                     print("%@", error ?? "no error")
                     guard
-                        let caiOutputs = outputs
+                        let caiOuputs = outputs
                         else {
                             print("Predict failed")
                             return
                     }
-                    if let caiOutput = caiOutputs.first {
-                        for concept in caiOutput.concepts {
+                    
+                    if let caiOutput = caiOuputs.first {
+                        var result = false
+                        for concept in caiOutput.concepts{
                             if concept.conceptName == "pizza" && concept.score >= 0.85 {
-                                
-                            }
-                            else {
-        
+                                result = true
                             }
                         }
+                        DispatchQueue.main.async {
+                            if result == true {
+                                self.resultLabel.text = "PIZZAAAAAAA"
+                            } else {
+                                self.resultLabel.text = "Not a pizza."
+                            }
+                        }
+                        
+                        
                     }
+                    
                 })
             })
-            
         }
-
     }
+
+
+}
+
     
+    
+extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]){
         let chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage
-        dismiss(animated:true, completion: nil)
-        
-        
-
+        self.imageView.image = chosenImage
+        recognizeImage(image: chosenImage)
+        resultLabel.text = "Analizing..."
+        dismiss(animated: true, completion: nil)
         
     }
+    
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
     }
-
 }
 
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    
-}
